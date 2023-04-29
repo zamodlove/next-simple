dd92949d307682d873b79c86456a8ed64150b21c
Kritsanachai Ngoennak (ITD # 1602) <Kritsanachai@tpppack.com>
2023-04-29 17:44:16 +0700

    NextJs V1.2

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import style from "@/styles/products.module.css";
 // https://dummyjson.com/products?limit=12
 
 export async function getStaticProps() {
@@ -18,11 +19,16 @@ export default function Index({ products }) {
         <meta name="Keywords" content="Zamod,ร้านค้า,BIOHAZARD" />
       </Head>
       <h1>สินค้าทั้งหมด</h1>
-      <div>
-        {products.map(item => (
+      <div className={styles.container}>
+        {products.map((item) => (
           <div key={item.id}>
-            <h2>{item.title}</h2>
-            <Image src={item.thumbnail} width={300} height={300} alt={item.title}/>
+            <h2 className={styles.container}>{item.title}</h2>
+            <Image
+              src={item.thumbnail}
+              width={300}
+              height={300}
+              alt={item.title}
+            />
           </div>
         ))}
       </div>
